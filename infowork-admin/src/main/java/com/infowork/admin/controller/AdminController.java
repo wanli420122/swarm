c09a35b68e7db49121cb3da8d50dabb7537789bf
qq790942967@163.com <mg420122>
2020-08-23 15:40:35 +0800

    11

--- infowork-admin/src/main/java/com/infowork/admin/controller/AdminController.java
+++ infowork-admin/src/main/java/com/infowork/admin/controller/AdminController.java
@@ -1,10 +1,12 @@
 package com.infowork.admin.controller;
 
 import com.infowork.admin.service.AdminService;
+import com.infowork.common.api.CommonResult;
 import com.infowork.common.domain.UserDTO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,10 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminController {
     @Autowired
     private AdminService adminService;
-    @ApiOperation("根据用户名获取用户信息")
+    @Value("${feign.okhttp.enabled}")
+    private String feginEnable;
+    @ApiOperation("根据用户名获取通用用户信息")
     @GetMapping("/loadByUsername")
     public UserDTO loadUserByUsername(@RequestParam String username){
         UserDTO userDTO= adminService.loadUserByUsername(username);
         return userDTO;
+    }
+    @ApiOperation("测试nacos的动态获取config配置信息")
+    @GetMapping("/testGetConfig")
+    public CommonResult<String> getConfig(){
+        return CommonResult.success(this.feginEnable);
     }
 }
